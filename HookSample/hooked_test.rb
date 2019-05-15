abb0d223420d5bb58c1bf0b31a544a4db49345b8
Takafumi Sugie <t.sugie@cb-cloud.com>
2019-05-15 11:14:01 +0900

    add result

--- HookSample/hooked_test.rb
+++ HookSample/hooked_test.rb
@@ -9,3 +9,10 @@ class HookedTest
 end
 
 HookedTest.new.test
+
+
+# result:
+# $ ruby hooked_test.rb
+# "hook method start: 2019-05-15 11:12:09 +0900"
+#"this is test."
+# "hook method end: 2019-05-15 11:12:09 +0900"
